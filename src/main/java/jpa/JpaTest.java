d954048d8740ace2b7d7f002bd3933f41033667d
Julian Descharreaux <julian.descharreaux@etudiant.univ-rennes1.fr>
2023-09-11 17:53:18 +0200

    Suppression departement employe + maj jpa

--- src/main/java/jpa/JpaTest.java
+++ src/main/java/jpa/JpaTest.java
@@ -58,20 +58,4 @@ public class JpaTest {
 			System.out.println("next rdv: " + rdv);
 		}
 	}
-	private void createEmployees() {
-		int numOfEmployees = manager.createQuery("Select a From Employee a", Employee.class).getResultList().size();
-		if (numOfEmployees == 0) {
-			Department department = new Department("java");
-			manager.persist(department);
-			manager.persist(new Employee("Jakab Gipsz", department));
-			manager.persist(new Employee("Captain Nemo", department));
-		}
-	}
-	private void listEmployees() {
-		List<Employee> resultList = manager.createQuery("Select a From Employee a", Employee.class).getResultList();
-		System.out.println("num of employess:" + resultList.size());
-		for (Employee next : resultList) {
-			System.out.println("next employee: " + next);
-		}
-	}
 }
